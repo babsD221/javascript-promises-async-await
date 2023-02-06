1090593d8b4365d4684a2575a0240c22c3e04cc1
Aboubacar DIAW <aboubacar.diaw@amadeus.com>
2023-02-06 11:16:03 +0100

    Run multiple promise

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { fetchBooks,fetchMovies } from './services';
 const movies = require('./data/movies.json');
 const books = require('./data/books.json');
 
-export function getBooksAndMovies() {
+const getBooksAndMovies = () => {
     return Promise.all([fetchBooks(), fetchMovies()]).then(([books, movies ]) => ({
         books,
         movies
@@ -10,14 +10,18 @@ export function getBooksAndMovies() {
       .catch(error => console.log("Error fetching books and movies", error));;
 }
 
-export function getBooksOrMovies() {
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results =>{
+     console.log('getBooksAndMoviesPromise',results)
+    });
+
+
+const getBooksOrMovies = () => {
     return Promise.race([fetchBooks(), fetchMovies()])
     .then(results => results)
     .catch(error => console.log("Error waiting for the promise race", error));
 }
 
-const getBooksAndMoviesPromise = getBooksAndMovies();
-getBooksAndMoviesPromise.then(results => console.log('getBooksAndMoviesPromise',results));
-
 const getBooksOrMoviesPromise = getBooksOrMovies();
-getBooksOrMoviesPromise.then(results, console.log('getBooksOrMoviesPromise',results));
+getBooksOrMoviesPromise.then(results => { console.log('getBooksOrMoviesPromise',results)
+});
